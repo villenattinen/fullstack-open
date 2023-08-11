8af40f168e1dcd80093a4f3022a3e85749d32876
Ville Nättinen <nattinen@synopsys.com>
2023-08-11 08:32:32 +0300

    Part4 Finish part 4.10

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,36 +1,26 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    const result = await blog.save()
+    response.status(201).json(result)
 })
 
-blogsRouter.put('/:id', (request, response) => {
+blogsRouter.put('/:id', async (request, response) => {
     const { likes } = request.body
-
-    Blog
+    const updatedPerson = await Blog
         .findByIdAndUpdate(
             request.params.id,
             { likes },
             { new: true, runValidators: true, context: 'query' }
         )
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
+    response.json(updatedPerson)
 })
 
 module.exports = blogsRouter
--- part4/tests/bloglist_api.test.js
+++ part4/tests/bloglist_api.test.js
@@ -1,26 +1,77 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+const initialBlogs = [
+    {
+        author: "kirjoittaja1",
+        likes: 1,
+        title: "blogi1",
+        url: "https://www.blogi1.fi/"
+    },
+    {
+        author: "kirjoittaja2",
+        likes: 2,
+        title: "blogi2",
+        url: "https://www.blogi2.fi/"
+    },
+    {
+        author: "kirjoittaja3",
+        likes: 3,
+        title: "blogi3",
+        url: "https://www.blogi3.fi/"
+    },
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[2])
+    await blogObject.save()
 })
 
-test('there is the right number of blogs', async () => {
-    const response = await api.get('/api/blogs')
+describe('API operations', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
 
-    expect(response.body).toHaveLength(3)
-})
+    test('there is the right number of blogs', async () => {
+        const response = await api.get('/api/blogs')
 
-test('blogs have \'id\' as identifier', async () => {
-    const response = await api.get('/api/blogs')
-    response.body.forEach((body) => {
-        expect(body.id).toBeDefined()
+        expect(response.body).toHaveLength(initialBlogs.length)
+    })
+
+    test('blogs have \'id\' as identifier', async () => {
+        const response = await api.get('/api/blogs')
+        response.body.forEach((body) => {
+            expect(body.id).toBeDefined()
+        })
+    })
+
+    test('blogs can be added through POST', async () => {
+        const newBlogObject = new Blog(
+            {
+                author: "kirjoittaja4",
+                likes: 4,
+                title: "blogi4",
+                url: "https://www.blogi4.fi/"
+            }
+        )
+        await api
+            .post('/api/blogs')
+            .send(newBlogObject)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(initialBlogs.length + 1)
     })
 })
 
