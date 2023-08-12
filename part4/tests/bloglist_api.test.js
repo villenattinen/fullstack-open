1e48aacc219be87104d802b1c0edbd1d8400c140
Ville Nättinen <villenattinen@gmail.com>
2023-08-12 09:52:42 +0300

    Refactor API tests

--- part4/tests/bloglist_api.test.js
+++ part4/tests/bloglist_api.test.js
@@ -15,89 +15,93 @@ beforeEach(async () => {
 })
 
 describe('API operations', () => {
-    test('blogs are returned as json', async () => {
-        await api
-            .get('/api/blogs')
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-    })
+    describe('fetching blogs with GET', () => {
+        test('blogs are returned as json', async () => {
+            await api
+                .get('/api/blogs')
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
+        })
 
-    test('there is the right number of blogs', async () => {
-        const response = await api.get('/api/blogs')
+        test('there is the right number of blogs', async () => {
+            const response = await api.get('/api/blogs')
 
-        expect(response.body).toHaveLength(helper.initialBlogs.length)
-    })
+            expect(response.body).toHaveLength(helper.initialBlogs.length)
+        })
 
-    test('blogs have \'id\' as identifier', async () => {
-        const response = await api.get('/api/blogs')
-        response.body.forEach((body) => {
-            expect(body.id).toBeDefined()
+        test('blogs have \'id\' as identifier', async () => {
+            const response = await api.get('/api/blogs')
+            response.body.forEach((body) => {
+                expect(body.id).toBeDefined()
+            })
         })
     })
 
-    test('blogs can be added through POST', async () => {
-        const newBlogObject = {
-            title: "blogi4",
-            author: "kirjoittaja4",
-            url: "https://www.blogi4.fi/",
-            likes: 4
-        }
+    describe('adding blogs through POST', () => {
+        test('blogs can be added through POST', async () => {
+            const newBlogObject = {
+                title: "blogi4",
+                author: "kirjoittaja4",
+                url: "https://www.blogi4.fi/",
+                likes: 4
+            }
 
-        await api
-            .post('/api/blogs')
-            .send(newBlogObject)
-            .expect(201)
+            await api
+                .post('/api/blogs')
+                .send(newBlogObject)
+                .expect(201)
 
-        const blogsAtEnd = await helper.blogsInDB()
-        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-    })
+            const blogsAtEnd = await helper.blogsInDB()
+            expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+        })
 
-    test('blogs are given default likes of 0', async () => {
-        const newBlogObject = {
-            author: "kirjoittajaX",
-            title: "blogiX",
-            url: "https://www.blogiX.fi/"
-        }
+        test('blogs are given default likes of 0', async () => {
+            const newBlogObject = {
+                author: "kirjoittajaX",
+                title: "blogiX",
+                url: "https://www.blogiX.fi/"
+            }
 
-        await api
-            .post('/api/blogs')
-            .send(newBlogObject)
-            .expect(201)
+            await api
+                .post('/api/blogs')
+                .send(newBlogObject)
+                .expect(201)
 
-        const blogsAtEnd = await helper.blogsInDB()
-        expect(blogsAtEnd[helper.initialBlogs.length].likes).toEqual(0)
-    })
+            const blogsAtEnd = await helper.blogsInDB()
+            expect(blogsAtEnd[helper.initialBlogs.length].likes).toEqual(0)
+        })
 
-    test('blog missing title value is not added', async () => {
-        const newBlogObject = {
-            author: "kirjoittaja4",
-            likes: 4,
-            url: "https://www.blogi4.fi/"
-        }
+        test('blog missing title value is not added', async () => {
+            const newBlogObject = {
+                author: "kirjoittaja4",
+                likes: 4,
+                url: "https://www.blogi4.fi/"
+            }
 
-        await api
-            .post('/api/blogs')
-            .send(newBlogObject)
-            .expect(400)
+            await api
+                .post('/api/blogs')
+                .send(newBlogObject)
+                .expect(400)
 
-        const blogsAtEnd = await helper.blogsInDB()
-        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-    })
+            const blogsAtEnd = await helper.blogsInDB()
+            expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+        })
 
-    test('blog missing url value is not added', async () => {
-        const newBlogObject = {
-            author: "kirjoittaja5",
-            likes: 5,
-            title: "blogi5",
-        }
+        test('blog missing url value is not added', async () => {
+            const newBlogObject = {
+                author: "kirjoittaja5",
+                likes: 5,
+                title: "blogi5",
+            }
 
-        await api
-            .post('/api/blogs')
-            .send(newBlogObject)
-            .expect(400)
+            await api
+                .post('/api/blogs')
+                .send(newBlogObject)
+                .expect(400)
 
-        const blogsAtEnd = await helper.blogsInDB()
-        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+            const blogsAtEnd = await helper.blogsInDB()
+            expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+        })
     })
 })
 
