143bdcddfeda49240d27040a4b6484987411a164
Ville Nättinen <villenattinen@gmail.com>
2023-10-01 20:08:23 +0300

    Part4 Finish task 4.15

--- part4/app.js
+++ part4/app.js
@@ -4,7 +4,8 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const notesRouter = require('./controllers/blogs')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const config = require('./utils/config')
 
 mongoose.connect(config.MONGODB_URI)
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
-app.use('/api/blogs', notesRouter)
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
