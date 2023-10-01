308ce284bc58524d9db7d9e4f05754d669987e42
Ville Nättinen <villenattinen@gmail.com>
2023-10-01 20:33:44 +0300

    Part4 Finish task 4.16

--- part4/tests/test_helper.js
+++ part4/tests/test_helper.js
@@ -1,31 +1,50 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
     {
         author: "kirjoittaja1",
         likes: 1,
         title: "blogi1",
-        url: "https://www.blogi1.fi/"
+        url: "https://www.blogi1.fi/",
     },
     {
         author: "kirjoittaja2",
         likes: 2,
         title: "blogi2",
-        url: "https://www.blogi2.fi/"
+        url: "https://www.blogi2.fi/",
     },
     {
         author: "kirjoittaja3",
         likes: 3,
         title: "blogi3",
-        url: "https://www.blogi3.fi/"
+        url: "https://www.blogi3.fi/",
     },
 ]
 
-const blogsInDB = async () => {
+const initialUsers = [
+    {
+        username: "käyttäjä1",
+        name: "matti",
+        password: "salasana"
+    },
+    {
+        username: "käyttäjä2",
+        name: "mikko",
+        password: "salis"
+    }
+]
+
+const blogsInDb = async () => {
     const blogs = await Blog.find({})
     return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+    const users = await User.find({})
+    return users.map(user => user.toJSON())
+  }
+
 module.exports = {
-    initialBlogs, blogsInDB
+    initialBlogs, initialUsers, blogsInDb, usersInDb
 }
