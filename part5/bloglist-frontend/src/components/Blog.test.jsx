9c43ce78ea30ac2229f9ca37b7b12806523fc3bb
Ville Nättinen <villenattinen@gmail.com>
2024-06-11 17:57:42 +0300

    Part5: Finish Task 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,7 @@
+import React from 'react'
+import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title and author, but not url and likes by default', () => {
@@ -18,8 +21,41 @@ test('renders title and author, but not url and likes by default', () => {
     />
   )
 
-  expect(screen.getByRole('button', { name: /view/i })).toBeDefined()
+  expect(screen.getByRole('button', { name: /view/ })).toBeDefined()
   expect(screen.getByText('Blog title Blog author')).toBeDefined()
   expect(screen.queryByText('Blog url')).toBeNull()
   expect(screen.queryByText('likes')).toBeNull()
+})
+
+test('renders also url and likes when blog view is expanded', async () => {
+  const blog = {
+    title: 'Blog title',
+    author: 'Blog author',
+    url: 'Blog url',
+    likes: 1,
+    user: {
+      username: 'username',
+      name: 'Firstname Lastname',
+    },
+  }
+
+  render(
+    <Blog
+      blog={blog}
+      username='username'
+      updateBlog={() => {}}
+      deleteBlog={() => {}}
+    />
+  )
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByRole('button', { name: /view/ })
+  await user.click(viewButton)
+
+  expect(screen.queryByRole('button', { name: /view/ })).toBeNull()
+  expect(screen.getByRole('button', { name: /hide/ })).toBeDefined()
+  expect(screen.getByText('Blog title Blog author')).toBeDefined()
+  expect(screen.getByText(/Blog url/)).toBeDefined()
+  expect(screen.getByText(/likes 1/)).toBeDefined()
+  expect(screen.getByText(/Firstname Lastname/)).toBeDefined()
 })
