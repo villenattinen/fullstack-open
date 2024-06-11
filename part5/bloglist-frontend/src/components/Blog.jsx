d50f519623cb243522e99cac520ae70a1e5967ec
Ville Nättinen <villenattinen@gmail.com>
2024-06-11 12:08:13 +0300

    Part5: Finish Task 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [isExtended, setIsExtended] = useState(false)
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        {!isExtended && <button onClick={() => (setIsExtended(!isExtended))}>view</button>}
+      </div>
+      {isExtended && 
+        <div>
+          {blog.url}
+          <br />
+          likes {blog.likes}
+          <button>like</button>
+          <br />
+          {blog.user?.name}
+          <br />
+          <button onClick={() => (setIsExtended(!isExtended))}>hide</button>
+        </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
