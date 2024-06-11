fc7ee7f8edfc2242ae94a919c78686c056870311
Ville Nättinen <villenattinen@gmail.com>
2024-06-11 18:04:32 +0300

    Part5: Finish Task 5.15

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import { test } from 'vitest'
 
 test('renders title and author, but not url and likes by default', () => {
   const blog = {
@@ -58,4 +59,33 @@ test('renders also url and likes when blog view is expanded', async () => {
   expect(screen.getByText(/Blog url/)).toBeDefined()
   expect(screen.getByText(/likes 1/)).toBeDefined()
   expect(screen.getByText(/Firstname Lastname/)).toBeDefined()
+})
+
+test('clicking the like button twice calls the event handler twice', async () => {
+  const blog = {
+    title: 'Blog title',
+    author: 'Blog author',
+    url: 'Blog url',
+    likes: 1,
+  }
+
+  const mockUpdateBlog = vi.fn()
+
+  render(
+    <Blog
+      blog={blog}
+      username='username'
+      updateBlog={mockUpdateBlog}
+      deleteBlog={() => {}}
+    />
+  )
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByRole('button', { name: /view/ })
+  await user.click(viewButton)
+  const likeButton = screen.getByRole('button', { name: /like/ })
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockUpdateBlog).toHaveBeenCalledTimes(2)
 })
