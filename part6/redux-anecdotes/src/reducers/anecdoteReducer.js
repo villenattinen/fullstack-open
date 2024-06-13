1a6c6337b0d6438afe78bc86163a5635ffc90eec
Ville Nättinen <villenattinen@gmail.com>
2024-06-13 22:03:42 +0300

    Part6: Finish Task 6.3

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -22,8 +22,15 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
-
-  return state
+  switch(action.type) {
+    case 'VOTE':
+      return state.map(anecdote => 
+        anecdote.id !== action.data.id 
+        ? anecdote 
+        : { ...anecdote, votes: anecdote.votes + 1 }
+      )
+    default: return state
+  }
 }
 
 export default reducer
