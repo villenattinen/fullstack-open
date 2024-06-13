377285845ed0d9eae4143f1a74ceddf2e5e5ef22
Ville Nättinen <villenattinen@gmail.com>
2024-06-13 22:55:53 +0300

    Part6: Finish Task 6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,30 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecodeList = () => {
+	const anecdotes = useSelector(state => state)
+	const dispatch = useDispatch()
+
+	const vote = (id) => {
+	console.log('vote', id)
+	dispatch(voteAnecdote(id))
+	}
+
+	return (
+		anecdotes.sort((firstAnecdote, secondAnecdote) => 
+		secondAnecdote.votes - firstAnecdote.votes
+		).map(anecdote =>
+			<div key={anecdote.id}>
+				<div>
+					{anecdote.content}
+				</div>
+				<div>
+					has {anecdote.votes}
+					<button onClick={() => vote(anecdote.id)}>vote</button>
+				</div>
+			</div>
+		)
+	)
+}
+
+export default AnecodeList
