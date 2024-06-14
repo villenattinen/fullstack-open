e15a8731168ad18362d5217491ac51d54c7ca66f
Ville Nättinen <villenattinen@gmail.com>
2024-06-14 18:11:00 +0300

    Part6: Finish Task 6.10

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,20 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit'
+
 const initialState = ''
 
-const filterReducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-  switch(action.type) {
-    case 'SET_FILTER':
-      return action.data.filter
-    default: return state
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setFilter(state, action) {
+      return action.payload
+    }
   }
-}
+})
 
-export const setFilter = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    data: { filter }
-  }
-}
-
-export default filterReducer
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
