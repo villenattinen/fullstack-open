e4840c535c8b30d7abe42496df86c1fe16cac7c1
Ville Nättinen <villenattinen@gmail.com>
2024-06-14 18:46:24 +0300

    Part6: Finish Task 6.12

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = ''
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState,
+})
+
+export default notificationSlice.reducer
